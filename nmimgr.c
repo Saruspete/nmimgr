50c46a40334e32b7cfa607b6a7f12d2f56c052cb
Adrien Mahieux <adrien@mahieux.net>
2017-02-25 15:38:45 +0100

    Code and doc cleanup

--- nmimgr.c
+++ nmimgr.c
@@ -1,21 +1,34 @@
 /**
- * NMI Manager - Module to skip / panic user-defined NMI codes
+ * NMI Manager
+ *
+ * This module allows you to Panic or Ignore specific NMI events.
  *
  * Adrien Mahieux <adrien.mahieux@gmail.com>
  *
+ *
  * Manage NMI events in a more fine-grained manner than "uknown_nmi_panic".
  * When a production host is unresponsive, we'd like to take a Kernel Dump.
- * If kdump is correctly setup, we need to crash the system for it to take
- * over. Usually, this is done by generating a NMI:
- * - openipmi chassis power diag
- * - vboxmanage debugvm "VMName" injectnmi
- * - 
+ * If kdump is correctly setup, we need to crash the system for it to start
  *
  * But if no handler registers the vendor-specific NMI event to trigger a
  * crash, the kernel logs a "Dazed and confused, but trying to continue"
  * message and server is still unresponsive.
  *
- * This module allows you to Panic or Ignore specific NMI events.
+ *
+ * Usage as a module: 
+ *  insmod nmimgr.ko events_panic=0,1,2,5-12,13,255 events_ignore=99
+ * Usage as Kernel builtin:
+ *   nmimgr.events_panic=0,1,2,5-12,13,255 nmimgr.events_ignore=99
+ *
+ * Parameters:
+ * - events_panic=LIST  Events to make the kernel Panic
+ * - events_ignore=LIST Events to drop, so no other handler can process them
+ *
+ *
+ * over. Usually, this is done by generating a NMI:
+ * - openipmi chassis power diag
+ * - vboxmanage debugvm "VMName" injectnmi
+ * - virsh inject-nmi "VMName"
  *
  * Usual NMI events (in decimal, to be used as module parameters):
  * - HP Ilo : 32,48
@@ -124,37 +137,23 @@ static int nmimgr_handle(unsigned int type, struct pt_regs *regs)
 static int nmimgr_register(void)
 {
 	int ret = 0;
+	int i;
 
-	/* We do register our handler first, as we only manage a specific list */
-	
-	//ret = register_nmi_handler(NMI_UNKNOWN, nmimgr_handle, NMI_FLAG_FIRST, NMIMGR_NAME);
-	ret = register_nmi_handler(NMI_UNKNOWN, nmimgr_handle, 0, NMIMGR_NAME);
-	if (ret) {
-		goto err1;
-		pr_warning(NMIMGR_NAME ": Unable to register NMI_UNKNOWN class\n");
+	for (i=1; i<NMI_MAX; i++) {
+		/* We do register our handler first, as we only manage a specific list */
+		ret = register_nmi_handler(i, nmimgr_handle, NMI_FLAG_FIRST, NMIMGR_NAME);
+		if (ret) {
+			pr_warning(NMIMGR_NAME ": Unable to register NMI class %d\n", i);
+			goto err;
+		}
 	}
-
-	ret = register_nmi_handler(NMI_SERR, nmimgr_handle, NMI_FLAG_FIRST, NMIMGR_NAME);
-	if (ret) {
-		goto err2;
-		pr_warning(NMIMGR_NAME ": Unable to register NMI_SERR class\n");
-	}
-
-	ret = register_nmi_handler(NMI_IO_CHECK, nmimgr_handle, NMI_FLAG_FIRST, NMIMGR_NAME);
-	if (ret) {
-		goto err3;
-		pr_warning(NMIMGR_NAME ": Unable to register NMI_IO_CHECK class\n");
-	}
-
 	pr_notice(NMIMGR_NAME ": Registered NMIs\n");
 	return 0;
 
-
-err3:
-	unregister_nmi_handler(NMI_SERR, NMIMGR_NAME);
-err2:
-	unregister_nmi_handler(NMI_UNKNOWN, NMIMGR_NAME);
-err1:
+err:
+	for(; i>0; i--) {
+		unregister_nmi_handler(i, NMIMGR_NAME);
+	}
 	return ret;
 }
 
@@ -179,12 +178,6 @@ static int __init nmimgr_setup_panic(char *str)
 	char *ret = 0;
 	
 	/* from lib/cmdline.c : Extract the int list from str into events_panic_list[] */
-/*
-	if (str && events_panic && events_panic[0] != 0)
-		pr_warning(NMIMGR_NAME": Skipping user-supplied param '%s' in favor of cmdline '%s'", str, events_panic);
-	else
-		events_panic = str;
-*/
 	pr_info(NMIMGR_NAME ": Setting panic from '%s'\n", events_panic);
 	ret = get_options(str, ARRAY_SIZE(events_panic_list), events_panic_list);
 	if (ret && *ret != 0) {
@@ -202,9 +195,7 @@ __setup("nmimgr.events_panic=", nmimgr_setup_panic);
 static int __init nmimgr_setup_ignore(char *str)
 {
 	char *ret = 0;
-/*
-	events_ignore = str;
-*/
+
 	pr_info(NMIMGR_NAME ": Setting ignore from '%s'\n", events_ignore);
 	ret = get_options(str, ARRAY_SIZE(events_ignore_list), events_ignore_list);
 	if (ret && *ret != 0) {
